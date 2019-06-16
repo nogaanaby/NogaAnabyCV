fc3a28773aaa690f9849ab4c9193a0cd52fc67a2
Noga Anaby <nogaanaby@gmail.com>
2019-06-16 18:44:31 +0300

    Add responsivness and windmill

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import switzerland from './assets/switzerland-no-sun.png';
+import switzerlandMobile from './assets/switzerland-mobile.png';
 import sun from './assets/sun.png';
 import cloude from './assets/cloude.png';
+import windmill from './assets/windmill.png';
+import windmillSpin from './assets/windmill-spiner.png';
 import Page1 from './Page1/PageOne';
 import Page2 from './Page2/PageTwo';
 import Pagination from './Pagination.js';
@@ -39,51 +42,13 @@ class App extends Component {
 
 
   flipPage = (direction) => {
-    if(direction === "next" && this.state.onPage === 1){
-      this.moveSunLeft()
-      this.moveSunDown()
-      this.setState({showErrows: false})
-    }
-    setTimeout(() => {
-      this.setState({onTypingAnimation: false})
-    }, 2500);
+    this.setState({onTypingAnimation: false})
     if(direction === "next" && this.state.onPage !== 3){
-      setTimeout(() => {
-        this.setState({onPage: this.state.onPage + 1, showErrows: true})
-      }, 2500);
+      this.setState({onPage: this.state.onPage + 1})
     }
     if(direction === "prev" && this.state.onPage !== 1){
-      setTimeout(() => {
-        this.setState({onPage: this.state.onPage - 1, showErrows: true})
-      }, 2500);
+      this.setState({onPage: this.state.onPage - 1})
     }
-  }
-
-  moveSunLeft = () => {
-    const x = this.state.sunPositionLeft
-    const screenWidth = window.innerWidth
-    this.setState({sunPositionLeft: x+1});
-    const moveLeftTimout = setTimeout(this.moveSunLeft, 5);
-    if(x >= screenWidth/3) {
-      clearTimeout(moveLeftTimout);
-    } 
-  }
-
-  moveSunDown = () => {
-    const screenWidth = window.innerWidth
-    const x = this.state.sunPositionLeft
-    const y = x * x/screenWidth + x/2 +1
-    this.setState({sunPositionTop: y})
-    const moveDownTimout = setTimeout(this.moveSunDown, 5);
-
-    const mountainWDH = 2300/506
-    const mountainWidth = 70 * screenWidth/100
-    const mountainsHeight= mountainWidth / mountainWDH
-    const maxHeight = window.innerHeight-mountainsHeight
-   //console.log('maxHeight:',  )
-    if(y >= maxHeight) {
-      clearTimeout(moveDownTimout);
-    } 
   }
 
 
@@ -106,10 +71,11 @@ class App extends Component {
             page={this.state.onPage}
             flipPage={this.flipPage}
             showErrows={this.state.showErrows}/>
-            <img src={sun} style={{top: this.state.sunPositionTop+ "px", left: this.state.sunPositionLeft+ "px"}} className="sun" alt="logo" />
-            <img src={cloude} style={{top: this.state.cloudePositionTop+"vh", right: this.state.cloudePositionRight+"em"}} className="cloude" alt="logo" />
-          <img src={switzerland} className="london" alt="logo" />
-          
+
+      <div className="windmill-container">
+        <img className="windmill-vector" src={windmill} />
+        <img className="windmill-spin" src={windmillSpin} />
+      </div>    
       </header>
     </div>
   );
